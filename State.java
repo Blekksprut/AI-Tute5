9370c36fe9d8cd3db96905aac704202b8132366d
Blekksprut <danielle.martin358@gmail.com>
2023-03-30 22:43:26 +1100

    returned state to default

--- State.java
+++ State.java
@@ -9,40 +9,14 @@ public class State
     //What cell was clicked to make this state, listed from 0 - 8
     //This is used by the GUI, but you could also use it for your search
     private int fMoveMade;
-    private int fCurrentTurn;
-    private int[] fGameState;
     /**
      * Find out in which cell the last move was made
      * 
      * @return - Represents where in the array the cell is
      */
-    private State fParent;
-
-    public State(int moveMade, int currentTurn, int[] gameState, State parent)
-    {
-        fMoveMade = moveMade;
-        fCurrentTurn = currentTurn;
-        fGameState = gameState;
-        fParent = parent;
-    }
-
-
-
-
 
     public int getfMoveMade() {
         return fMoveMade;
     }
 
-    public int getfCurrentTurn() {
-        return fCurrentTurn;
-    }
-
-    public int[] getfGameState() {
-        return fGameState;
-    }
-
-    public State getfParent() {
-        return fParent;
-    }
 }
